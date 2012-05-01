551ccfc599fa63cdc0621b3d55195ec9cf163903
David Hodo <david.hodo@gmail.com>
2012-04-30 23:07:25 -0500

    starting to stub out application

--- include/novatel/novatel.h
+++ include/novatel/novatel.h
@@ -1,25 +1,61 @@
+/*!
+ * \file novatel/novatel.h
+ * \author David Hodo <david.hodo@gmail.com>
+ * \version 1.0
+ *
+ * \section LICENSE
+ *
+ * The BSD License
+ *
+ * Copyright (c) 2011 David Hodo - Integrated Solutions for Systems (IS4S)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * \section DESCRIPTION
+ *
+ * This provides an interface for OEM 4, V, and 6 series of Novatel GPS receivers
+ *
+ * This library depends on CMake-2.4.6 or later: http://www.cmake.org/
+ * This library depends on Serial: https://github.com/wjwwood/serial
+ *
+ */
+
 #ifndef NOVATEL_H
 #define NOVATEL_H
 
-#define BOOST_ASIO_NO_WIN32_LEAN_AND_MEAN
-#include "novatel_structures.h"
+#include <string>
 
-// define message ids
-#define NEWINSPVA 0
-#define NEWRXHWLEVELS 1
-#define NEWBESTUTM 2
-#define NEWRAWIMU 3
-#define NEWBESTPOS 4
-#define NEWBESTVELOCITY	5
-#define NEWINSSPD 6
-#define NEWINSUTM 7
-#define NEWRXSTATUS 8
-#define NEWRXSTATUSEVENT 9
-#define	NEWVEHICLEBODYROTATION 10
-#define NEWRAWIMUS 11
-#define NEWBSLNXYZ 12
-#define NEWPSRXYZ 13
-#define CMDACK 14
+// Structure definition headers
+#include "novatel/novatel_enums.h"
+#include "novatel/novatel_structures.h"
+// Boost Headers
+#include <boost/function.hpp>
+#include <boost/thread.hpp>
+#include <boost/bind.hpp>
+// Serial Headers
+#include "serial/serial.h"
+
+namespace novatel{
+
+
+typedef boost::function<double()> GetTimeCallback;
+
 
 
 class Novatel
@@ -28,209 +64,96 @@ public:
 	Novatel();
 	~Novatel();
 
-	//************************************************************************
-	//! array of event handles to signal when new messages have arrived
-	//HANDLE hNovatelEvents[15];
+	/*!
+	 * Connects to the Novatel receiver given a serial port.
+	 *
+	 * @param port Defines which serial port to connect to in serial mode.
+	 * Examples: Linux - "/dev/ttyS0" Windows - "COM1"
+	 *
+	 * @throws ConnectionFailedException connection attempt failed.
+	 * @throws UnknownErrorCodeException unknown error code returned.
+	 */
+	bool Connect(std::string port, int baudrate=115200, long timeout=50);
 
-	//! Initializes com port
-	/*! Creates instance of com port object
-	 *  \param comPort com port number to listen on
-	 *	\param baudRate
-	 *  \return true if initialized successfully, false otherwise */
-	//bool Initialize(int comPortNumber, int baudRate);
-	//bool Initialize(int comPortNumber, int baudRate, int byteSize,int parity,int stopSits);
-	//! Starts listening for incoming data
-	//bool StartReading();
-	//! stops listening for incoming data
-	//bool StopReading();
+   /*!
+    * Disconnects from the serial port
+    */
+    void Disconnect();
 
-	//! shutsdown device and closes serial port
-	//bool Shutdown();
-
-	//! Indicates whether object has been initialized successfully or not
-	/*! Returns true if successfully initialized, false otherwise */
-	//bool isActive(){return bActive;};
-	//! Indicates status of read thread
-	/*! Returns true if read thread is running, false otherwise */
-	//bool isReading(){return bReading;};
-
-	//! Sends the specified command to the SPAN and waits for an acknowledgement
-	//bool SendMessage(char* msg, unsigned int length);
-
-	//bool SendString(char* msg);
+    /*!
+     * Pings the GPS to determine if it is properly connected
+     *
+     * This method sends a ping to the GPS and waits for a response.
+     *
+     * @param num_attempts The number of times to ping the device
+     * before giving up
+     * @param timeout The time in milliseconds to wait for each reponse
+     *
+     * @return True if the GPS was found, false if it was not.
+     */
+     bool Ping(int num_attempts=5, long timeout=100);
 
 
-	//************************************************************************
-	// PUBLIC METHODS TO GET LAST READ LOGS
-	//! Method to get structure containing last BESTPOS log
-	BestPosition getLastBestPOS();
-	//! Method to get structure containing last BestUTM log
-	BestUTMPosition getLastBestUTM();
-	//! Method to get structure containing last INSPVA log
-	INSPVA getLastINSPosVelAtt();
-	//! Method to get structure containing last PsrXYZ log
-	PsrXYZ getLastPsrXYZ();
-	//! Method to get structure containing last INSPVA log
-	INSPVAS getLastINSPosVelAttShort();
-	//! Method to get last baseline message
-	bslnxyz getLastBslnXYZ();
-	//! Method to get structure containing last RAWIMU log
-	RawIMU getLastRawIMU();
-	//! Method to get structure containing last RAWIMUS log
-	RawIMUS getLastRawIMUS();
-	//! Method to get structure containing last VEHICLEBODYROTATION log
-	VehicleBodyRotation getLastVehicleBodyRotation();
-	//! Method to get structure containing last VEHICLEBODYROTATION log
-	BestVelocity getLastBestVelocity();
-	//! Method to get structure containing last INSSPD log
-	INSSPD getLastINSSpeed();
-	//! Method to get structure containing last INSUTM log
-	INSUTM getLastINSUTM();
-	//! Method to get structure containing last RXStatus log
-	RXStatus getLastRXStatus();
-	//! Method to get structure containing last RXStatusEvent log
-	RXStatusEvent getLastRXStatusEvent();
-	//! Method to get structure containing last RXHWLevels log
-	RXHwLevels getLastRXHwLevels();
+     /*!
+      * Pings the GPS to determine if it is properly connected
+      *
+      * This method sends a ping to the GPS and waits for a response.
+      *
+      * @param num_attempts The number of times to ping the device
+      * before giving up
+      * @param timeout The time in milliseconds to wait for each reponse
+      *
+      * @return True if the GPS was found, false if it was not.
+      */
+     void set_time_handler(GetTimeCallback time_handler) {
+         this->time_handler_ = time_handler;
+     }
 
-	//************************************************************************
-	// SPAN COMMANDS
-	//! Send command to SPAN to stop sending all logs
-	bool UnlogAll();
+	void UnlogAll();
 
-	NOUT_ID DecodeBinaryID(unsigned int msgID);  // Member function to identify the log
-
-	void InitializeEvents();
 
 private:
 
-	void bufferIncomingData(unsigned char* msg, unsigned int length);
-	//! Function to parse logs into a usable structure
-	void ParseLog(unsigned char* log, NOUT_ID logID);
-	unsigned char dataBuf[MAX_NOUT_SIZE];
-	unsigned char* dataRead;
-	unsigned int bytesRemaining;
-	unsigned int bufIndex;
+	/*!
+	 * Starts a thread to continuously read from the serial port.
+	 *
+	 * Starts a thread that runs 'ReadSerialPort' which constatly reads
+	 * from the serial port.  When valid data is received, parse and then
+	 *  the data callback functions are called.
+	 *
+	 * @see xbow440::DataCallback, xbow440::XBOW440::ReadSerialPort, xbow440::XBOW440::StopReading
+	 */
+	void StartReading();
 
-	bool readingACK;
- 
-	void opened();
-	void closed();
-	bool startedReading();
-	bool stoppedReading();
+	/*!
+	 * Starts the thread that reads from the serial port
+	 *
+	 * @see xbow440::XBOW440::ReadSerialPort, xbow440::XBOW440::StartReading
+	 */
+	void StopReading();
 
-	//*******************************************************************
-	//DATA STRUCTURES TO HOLD LAST READ LOGS
-	//! Last received BESTPOS log
-	BestPosition curBestPosition;
-	//! Mutex to control access to bestgpspos structure
-//	HANDLE hBestPOSMutex;
-//
-//	//! Last received INSPVA log
-//	INSPVA curINSPosVelAtt;
-//	//! Mutex to control access to INSPVA structure
-//	HANDLE hINSPVAMutex;
-//
-//	//! Last received PsrXYZ log
-//	PsrXYZ curPsrXYZ;
-//	//! Mutex to control access to PsrXYZ structure
-//	HANDLE hPsrXYZMutex;
-//
-//	//! Last received INSPVAS log
-//	INSPVAS curINSPosVelAttShort;
-//	//! Mutex to control access to INSPVAS structure
-//	HANDLE hINSPVASMutex;
-//
-//	//! Last received BslnXYZ log
-//	bslnxyz curBslnXYZ;
-//	//! Mutex to control access to BslnXYZ structure
-//	HANDLE hBslnXYZMutex;
-//
-//	//! Last received RAWIMU log
-//	RawIMU curRawIMU;
-//	//! Mutex to control access to RAWIMU structure
-//	HANDLE hRawIMUMutex;
-//
-//	//! Last received RAWIMUS log
-//	RawIMUS curRawIMUS;
-//	//! Mutex to control access to RAWIMUS structure
-//	HANDLE hRawIMUSMutex;
-//
-//
-//	//! Last received BESTUTM log
-//	BestUTMPosition curBestUTMPosition;
-//	//! Mutex to control access to bestgpspos structure
-//	HANDLE hBestUTMMutex;
-//
-//	//! Last received VehicleBodyRotation log
-//	VehicleBodyRotation curVehicleBodyRotation;
-//	//! Mutex to control access to VehicleBodyRotation structure
-//	HANDLE hVehicleBodyRotationMutex;
-//
-//	//! Last received BestVelocity log
-//	BestVelocity curBestVelocity;
-//	//! Mutex to control access to BestVelocity structure
-//	HANDLE hBestVelocityMutex;
-//
-//	//! Last received INSSPD log
-//	INSSPD curINSSPD;
-//	//! Mutex to control access to INSSPD structure
-//	HANDLE hINSSPDMutex;
-//
-//	//! Last received INSUTM log
-//	INSUTM curINSUTM;
-//	//! Mutex to control access to INSUTM structure
-//	HANDLE hINSUTMMutex;
-//
-//	//! Last received RXStatus log
-//	RXStatus curRXStatus;
-//	//! Mutex to control access to RXStatus structure
-//	HANDLE hRXStatusMutex;
-//
-//	//! Last received RXStatusEvent log
-//	RXStatusEvent curRXStatusEvent;
-//	//! Mutex to control access to RXStatusEvent structure
-//	HANDLE hRXStatusEventMutex;
-//
-//	//! Last received RXHwLevels log
-//	RXHwLevels curRXHwLevels;
-//	//! Mutex to control access to RXHwLevels structure
-//	HANDLE hRXHwLevelsMutex;
+	/*!
+	 * Method run in a seperate thread that continuously reads from the
+	 * serial port.  When a complete packet is received, the parse
+	 * method is called to process the data
+	 *
+	 * @see xbow440::XBOW440::Parse, xbow440::XBOW440::StartReading, xbow440::XBOW440::StopReading
+	 */
+	void ReadSerialPort();
 
-	int hdrLength;
-	unsigned int msgID;
 
-	//! Defines an object to provide low level interaction with the serial port
-	//com_port_event_driven comPort;
+	/*!
+	 * Parses a packet of data from the GPS.  The
+	 */
+	void Parse(char *data, size_t length);
 
-	//int portNumber;
-	//int baudRate;
-
-	//! Function to start thread to listen for incoming com data
-	/*! static function used to call thread to handle new span measurements
-	 *  these are necessary because the callback function used to create a thread
-	 *	cannot call a member function, because member functions are passed a hidden
-	 *	argument (this), workaround using a static method and pointer to current object */
-	//static DWORD WINAPI ComPortListenThread(LPVOID Parameter)
-	//{
-	//	// cast pointer and call actual thread method(which is a member function)
-	//	((Novatel*)Parameter)->ComListen();
-	//	return 1;
-	//};
-
-	////! thread function to read incoming com messages and parse them into a message structure
-	//void ComListen();
-
-	////! handle to read thread
-	//HANDLE hReadThread;
-	////! handle to event to signal that read thread has started
-	//HANDLE hReadStarted;
-
-	////! indicates whether com port has been initialized or not
-	//bool bActive;
-	////! indicates whether read thread is running or not
-	//bool bReading;
+	//! Serial port object for communicating with sensor
+	serial::Serial *serial_port_;
+	//! shared pointer to Boost thread for listening for data from novatel
+	boost::shared_ptr<boost::thread> read_thread_ptr_;
+	bool reading_status_;  //!< True if the read thread is running, false otherwise.
+	GetTimeCallback time_handler_; //!< Function pointer to callback function for timestamping
 
 };
-
+}
 #endif
