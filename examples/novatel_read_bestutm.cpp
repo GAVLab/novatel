1027439506048c2d29f8b604a39f9f86a7d2248d
David Hodo <david.hodo@gmail.com>
2013-05-23 16:05:08 -0500

    add vel message to example

--- examples/novatel_read_bestutm.cpp
+++ examples/novatel_read_bestutm.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <sstream>
 #include <boost/thread.hpp>
+#include <signal.h>
 
 #include "novatel/novatel.h"
 using namespace novatel;
@@ -18,8 +19,24 @@ void BestUtmHandler(UtmPosition &pos, double &timestamp) {
             ","<<pos.northing<<")" << std::endl;
 }
 
+void BestVelHandler(Velocity &vel, double &timestamp) {
+    std::cout << "[" << setprecision(2) << std::fixed << timestamp << 
+        setprecision(std::cout.precision()) <<  "] BestVel: " << 
+        vel.header.gps_week << ":" << vel.header.gps_millisecs << 
+        "   Horiz: " << vel.horizontal_speed << " Course: "<<
+        vel.track_over_ground<<std::endl;
+}
+
+void sighandler(int sig) {
+    std::cout << "Exiting." << std::endl;
+    exit(EXIT_SUCCESS);    
+}
+
 int main(int argc, char **argv)
 {
+    // catch ctrl-c
+    signal(SIGINT, &sighandler);
+
     if(argc < 3) {
         std::cerr << "Usage: novatel_example <serial port address> <baud rate>" << std::endl;
         return 0;
@@ -41,7 +58,8 @@ int main(int argc, char **argv)
     }
 
     my_gps.set_best_utm_position_callback(&BestUtmHandler);
-    my_gps.ConfigureLogs("BESTUTMB ONTIME 0.1");
+    my_gps.set_best_velocity_callback(&BestVelHandler);
+    my_gps.ConfigureLogs("BESTUTMB ONTIME 0.1;BESTVELB ONTIME 0.1");
 
     while(1) {
         boost::this_thread::sleep(boost::posix_time::milliseconds(10000));
